76388e70807e2c3cb645d7b5f45cde1ed09684dc
ss12@umbc.edu <ss12@umbc.edu>
2023-02-26 15:15:07 -0500

    added more code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,32 @@
 import { Component } from '@angular/core';
+import { User } from './interface/user';
+import { UserService } from './service/user.service';
+import {HttpClient} from '@angular/common/http'
+import { EMPTY } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
   title = 'angularrestapi';
+  listofusers:User[]=[];
+  singleuser!:User;
+  
+ constructor (private userSerivce: UserService){}
+
+ onGetUsers():void{
+  this.userSerivce.getUsers().subscribe((users)=>{
+    this.listofusers=users
+  });
+ }
+
+ onGetUser():void{
+  this.userSerivce.getUser().subscribe((user)=>{
+    this.singleuser=user
+  });
+ }
+
 }
